aafc3ba5069f954807263ca7ce76d3dfdd613fd0
Bmax <binh27112004@gmail.com>
2019-08-08 16:10:42 +0700

    xxx

--- resources/views/home/baiviet.blade.php
+++ resources/views/home/baiviet.blade.php
@@ -16,12 +16,22 @@
     .kt-header--fixed.kt-subheader--fixed.kt-subheader--enabled .kt-wrapper{
         padding-top: 0;
     }
+    .resize{
+         display: block;
+  max-width:230px;
+  max-height:95px;
+  width: auto;
+  height: auto;
+    }
 </style>
 <div class="container mb-5 " style="padding-bottom: 4.3rem;">
     <div class="row">
         <div class="col-12">
             <a href="{{route('trangblog')}}"><i class="flaticon2-back"></i>Back</a>
-            <h1 class="pt-5">{{$blog->title}}</h1>
+            <br>
+
+            <h1 class="pt-5">{{$blog->title}}</h1>.
+            <img class="resize" src="{{asset('public/thumbnailblog')}}/{{$blog->img}}" />
             <h3><i class="flaticon-eye"></i> {{$blog->view}}</h3>
             <h5>Đăng vào ngày {{$blog->created_at}}</h5>
             <h5>Cập nhật lần cuối {{$blog->updated_at}}</h5>
